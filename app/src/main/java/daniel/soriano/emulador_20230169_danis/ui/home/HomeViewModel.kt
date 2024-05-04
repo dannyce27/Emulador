3fddc940fd41721406219b7dd37bf05a6f603073
daniel <daniel@DESKTOP-6UJDGOB>
2024-05-03 22:50:15 -0600

    cree el proyecto

---
+++ app/src/main/java/daniel/soriano/emulador_20230169_danis/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package daniel.soriano.emulador_20230169_danis.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
